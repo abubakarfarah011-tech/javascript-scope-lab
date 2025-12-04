39438b98d935a16630e0b74ec8bdb288b2aa4301
abubakar <abubakarfarah011@gmail.com>
2025-12-04 22:02:17 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+const burgers = [`Hamburger`,`Cheeseburger`]
+
+const featuredDrinks =`Strawberry Milkshake`;
+
+
+function addBurger(){
+    const newburger = `Flatburger`;
+    return burgers.push(newburger);
+}
+
+if (true) {
+    const anotherNewBurger =`Maple Bacon Burger`;
+    burgers.push(anotherNewBurger);
+}
+
+ function changeFeaturedDrink(){
+    featuredDrinks = `The JavaShake`;
+ 
+}
