fe1b3e3aaa1f14f55beab1c839d89a4141c45bd5
abubakar <abubakarfarah011@gmail.com>
2025-12-05 14:35:14 +0300

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 // Write your solution in this file!
-
-const burgers = [`Hamburger`,`Cheeseburger`]
-
-const featuredDrinks =`Strawberry Milkshake`;
-
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink ="Strawberry Milkshake";
 
 function addBurger(){
-    const newburger = `Flatburger`;
-    return burgers.push(newburger);
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
 }
-
-if (true) {
-    const anotherNewBurger =`Maple Bacon Burger`;
+if(true){
+    const anotherNewBurger ='Maple Bacon Burger';
     burgers.push(anotherNewBurger);
 }
+ console.log(burgers);
 
- function changeFeaturedDrink(){
-    featuredDrinks = `The JavaShake`;
- 
+function changeFeaturedDrink(){
+    featuredDrink ='The JavaShake'
 }
